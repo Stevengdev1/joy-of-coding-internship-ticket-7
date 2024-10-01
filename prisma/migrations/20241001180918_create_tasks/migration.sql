0c0d4c36d178e12e05c5623af8ea41ddf5ed6bb7
Stevengdev1 <stevendg922@gmail.com>
2024-10-01 17:29:08 -0400

    Created the tasks model.

---
+++ prisma/migrations/20241001180918_create_tasks/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Tasks` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `status` VARCHAR(191) NOT NULL DEFAULT 'pending',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `dueDate` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
