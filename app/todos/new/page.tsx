ac876fcb8c6b70283c564b51e5a4457b2feed499
Stevengdev1 <stevendg922@gmail.com>
2024-10-12 16:27:57 -0400

    Installed React SimpleMDE editor and added markdown editor.

--- app/todos/new/page.tsx
+++ app/todos/new/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import { Button, TextField } from "@radix-ui/themes";
-import { TextArea } from "@radix-ui/themes";
-import React from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewTaskpage = () => {
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root placeholder="Enter your task ..."></TextField.Root>
-      <TextArea placeholder="Reply to comment…" />
+      <SimpleMDE placeholder="Description" />
       <Button>Submit New Task</Button>
 
     </div>
