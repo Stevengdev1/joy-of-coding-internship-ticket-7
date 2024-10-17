f98d42df494184aefcc50137547db229d848dc14
Stevengdev1 <stevendg922@gmail.com>
2024-10-16 21:01:38 -0400

    Installed react-hook-form, installed axios to send data to the API and completed the handling of the task submission form .

--- app/todos/new/page.tsx
+++ app/todos/new/page.tsx
@@ -2,16 +2,42 @@
 
 import { Button, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+
+interface TaskForm {
+  title: string;
+  description: string;
+}
 
 const NewTaskpage = () => {
-  return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Enter your task ..."></TextField.Root>
-      <SimpleMDE placeholder="Description" />
-      <Button>Submit New Task</Button>
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<TaskForm>();
 
-    </div>
+  return (
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/tasks", data);
+        router.push("/todos");
+      })}
+    >
+      <TextField.Root
+        placeholder="Enter your task ..."
+        {...register("title")}
+      ></TextField.Root>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
+
+      <Button>Submit New Task</Button>
+    </form>
   );
 };
 
